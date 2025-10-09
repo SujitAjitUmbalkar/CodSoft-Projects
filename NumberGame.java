4c3b2384049a277b56c02ec62a0c1575d683bdbe
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-10-09 18:41:15 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,7 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGame {
+public class NumberGame 
+   {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
